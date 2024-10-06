162102d4df9bda6ac16078c898fd56987aafd16b
manujunqueira <manuelajunqueira27@gmail.com>
2024-10-06 16:37:43 -0400

    "Create Department class to manage employees and calculate salary"

--- employee_management.js
+++ employee_management.js
@@ -11,8 +11,25 @@ class Employee {
     getDetails(){ // getting and returning the employee's details
         return (`${this.name} earns ${this.salary} working as a ${this.position}.`);
     }
-}
+};
 
 const bia = new Employee("Bia", 75000, "Designer"); //adding a new employee and logging their details
 console.log(bia.getDetails());
+
+// Task 2 - Create a Department Class
+
+class Department {
+    constructor (name){
+        this.name = name;
+        this.employees = [] //empty employees array to add objects to 
+    }
+
+    addEmployee(employee){
+        this.employees.push(employee); // pushing employees into empty array
+    }
+
+    get getDepartmentSalary(){
+        return this.employees.reduce((totalSalary, employee) => totalSalary + employeeSalary, 0);
+    } // calculate total salary with reduce method 
+};
 
